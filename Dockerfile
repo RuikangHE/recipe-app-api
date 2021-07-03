827956dd214f44590930565758f896062ab0714a
Ruikang <heyry004@mymail.unisa.edu.au>
2021-07-03 15:46:41 +0930

    tweak

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.7-alpine
-MAINTAINER Intern Eric
+LABEL maintainer="eric2369158615@gmail.com"
 
 ENV PYTHONUNBUFFERED 1
 
--- app/core/models.py
+++ app/core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user"""
+        """Creates and saves a new User"""
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
